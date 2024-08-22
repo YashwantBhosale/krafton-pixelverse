6329183cc809e37758a643950a16bb15f4ce9cfd
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2024-08-22 19:46:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const vantaEffect = VANTA.WAVES({
+  el: "#vanta-canvas",
+  mouseControls: false,
+  touchControls: false,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1,
+  scaleMobile: 1.0,
+  color: 0x00001,
+  shininess: 10.0,
+  waveHeight: 6.5,
+  zoom: 1.1,
+});
+
+// Function to create a glitchy effect by varying the shininess
+function glitchShininess() {
+  const minShininess = 15;
+  const maxShininess = 25;
+
+  // Generate a random shininess value within the range
+  const randomShininess =
+    Math.random() * (maxShininess - minShininess) + minShininess;
+
+  // Apply the new shininess value to the VANTA effect
+  vantaEffect.setOptions({ shininess: randomShininess });
+
+  // Schedule the next glitch effect
+  setTimeout(glitchShininess, Math.random() * 300); // Random interval between glitches
+}
+
+// Start the glitch effect loop
+glitchShininess();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const glitchText = document.getElementById('glitch-text');
+    const textContent = glitchText.textContent;
+    glitchText.innerHTML = '';
+  
+    // Split the text into individual span elements
+    textContent.split('').forEach((char) => {
+      const span = document.createElement('span');
+      span.classList.add('glitch-char');
+      span.textContent = char;
+      glitchText.appendChild(span);
+    });
+  
+    const glitchChars = document.querySelectorAll('.glitch-char');
+  
+    function randomizeOpacity() {
+      glitchChars.forEach((char) => {
+        char.style.opacity = Math.random();  // Random opacity for each character
+        setTimeout(() => {
+          char.style.opacity = 1;  // Settle to full opacity after random time
+        }, Math.random() * 2000);  // Random delay before settling
+      });
+    }
+  
+    randomizeOpacity();
+  });
+  
